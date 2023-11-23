477c3cdb0910629ce00e1e80d5dea951c514b361
Olimov Amir <olimovamir086@gmail.com>
2023-11-23 13:15:01 +0300

    start

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+
+
+class FirebaseController extends Controller
+{
+    public function getData()
+    {
+        
+
+        
+    }
+}
